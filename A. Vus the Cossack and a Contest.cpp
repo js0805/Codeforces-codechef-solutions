3c915e542342e4b45935fb6f4899f56a9d45bee1
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-04-30 23:22:04 +0530

    Create A. Vus the Cossack and a Contest.cpp

---
+++ A. Vus the Cossack and a Contest.cpp
@@ -0,0 +1,24 @@
+//https://codeforces.com/problemset/problem/1186/A
+//created by js0805
+#include<bits/stdc++.h>
+#include<string>
+#include <algorithm> 
+#include<vector>
+using namespace std;
+void solve()
+{
+   int n,m,k;
+   cin >>n>>m>>k;
+   if(n>m || n>k)
+      cout <<"No";
+   else 
+      cout<<"Yes";
+ 
+}
+int main()
+{
+ios_base::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+solve();
+}
