35c6d61ee4f8132c855e3b72a1c13326252d5bd9
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-04-30 18:46:09 +0530

    Create A. In Search of an Easy Problem.cpp

---
+++ A. In Search of an Easy Problem.cpp
@@ -0,0 +1,34 @@
+//https://codeforces.com/problemset/problem/1030/A
+
+#include<bits/stdc++.h>
+#include<string>
+#include <algorithm> 
+#include<vector>
+using namespace std;
+void solve()
+{
+   int n;
+   cin >>n;
+   int arr[n];
+   int a=0;
+   for(int i =0;i<n;i++)
+   {
+      cin >>arr[i];
+      if(arr[i]==1)
+      
+         a++;
+   }
+  // cout << a<< " " << b << endl;
+   if(a==0)
+      cout << "easy";
+   else 
+      cout << "hard";
+ 
+}
+int main()
+{
+ios_base::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+solve();
+}
