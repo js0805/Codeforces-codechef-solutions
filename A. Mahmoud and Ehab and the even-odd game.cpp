5a8cf86ba8523ba0f6a087ab391c1185b807a71c
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-04-30 23:00:40 +0530

    Create A. Mahmoud and Ehab and the even-odd game.cpp

---
+++ A. Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,27 @@
+//https://codeforces.com/contest/959/problem/A
+//created by js0805
+#include<bits/stdc++.h>
+#include<string>
+#include <algorithm> 
+#include<vector>
+using namespace std;
+void solve()
+{
+   int t;
+   cin >>t;
+   if(t%2==0)
+   {
+      cout << "Mahmoud";
+ 
+   }
+   else
+      cout << "Ehab";
+ 
+}
+int main()
+{
+ios_base::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+solve();
+}
