3bb13b5ee47856fc238fd55d6607982a48f0b9f1
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-05-03 00:42:27 +0530

    Create A. Domino Piling.cpp

---
+++ A. Domino Piling.cpp
@@ -0,0 +1,29 @@
+//https://codeforces.com/problemset/problem/50/A
+//created by js0805
+#include<algorithm>
+#include<string>
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+#define ll long long
+void solve()
+{
+    ll m,n;
+    cin >>m>>n;
+    int t = m*n;
+    int a = 2;
+    cout << t/2;
+
+
+
+   
+}
+int main()
+{
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+   solve();
+
+   
+}
