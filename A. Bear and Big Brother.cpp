bff7f075444f6b87be90fdcdaa5a90cce47810be
amreenabbas <50434514+amreenabbas@users.noreply.github.com>
2020-05-02 18:25:24 +0530

    Update A. Bear and Big Brother.cpp

--- A. Bear and Big Brother.cpp
+++ A. Bear and Big Brother.cpp
@@ -29,3 +29,33 @@ cin.tie(0);
 cout.tie(0);
 solve();
 }
+
+
+//created by AA
+
+#include<bits/stdc++.h>
+#include<math.h>
+#include <algorithm> 
+using namespace std;
+void solve()
+{
+   int a,b;
+   cin >>a>>b;
+   double d;
+   double h = (double)b/a;
+  
+   double lg = (log (h))/(log(1.5));
+   double diff = lg-floor(lg);
+   if(diff == 0.0)
+   d = ceil(lg)+1;
+   else
+   d = ceil(lg);
+    cout << d;
+}
+int main()
+{
+ios_base::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+solve();
+}
