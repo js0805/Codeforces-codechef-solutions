b70a7e5d87f96ac3312e09942edbcad53ee2ad2a
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-05-02 23:15:05 +0530

    Create A. Watermelon.cpp

---
+++ A. Watermelon.cpp
@@ -0,0 +1,32 @@
+//https://codeforces.com/problemset/problem/4/A
+//created by js0805
+#include<bits/stdc++.h>
+#include<string>
+#include <algorithm> 
+#include<vector>
+using namespace std;
+#define ll long long int
+void solve()
+{
+   ll a;
+   cin >>a;
+   if(a%2!=0 || a==2)
+      cout << "NO";
+   else
+      cout <<"YES";
+
+   
+   
+}
+
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+solve();
+
+
+}
+ 
