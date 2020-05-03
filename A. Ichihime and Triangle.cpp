7707b6dc88f307076fdff6218b971817639ef48a
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-05-03 16:12:25 +0530

    Create A. Ichihime and Triangle.cpp

---
+++ A. Ichihime and Triangle.cpp
@@ -0,0 +1,33 @@
+//https://codeforces.com/contest/1337/problem/A
+//created by js0805
+#include<algorithm>
+#include<string>
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+#define ll long long
+void solve()
+{
+   ll a,b,c,d;
+   cin >>a>>b>>c>>d;
+   cout << b << " "<< c <<" "<<c;
+   cout <<endl;
+   
+     
+ 
+ 
+   
+}
+int main()
+{
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+   ll t;
+   cin >>t;
+   for(int i=1;i<=t;i++)
+      {
+   solve();
+ 
+}   
+}
