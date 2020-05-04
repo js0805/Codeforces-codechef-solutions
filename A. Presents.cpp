975029d3313bd9e68991f8048815623ddaf240aa
amreenabbas <50434514+amreenabbas@users.noreply.github.com>
2020-05-04 12:00:08 +0530

    Update A. Presents.cpp

--- A. Presents.cpp
+++ A. Presents.cpp
@@ -46,3 +46,44 @@ int main()
 
 //}   
 }
+
+
+
+
+
+//created by AA
+
+#include<algorithm>
+#include<string>
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+
+void solve()
+{
+   int n;
+   cin >> n;
+   int arr[n+1],i;
+   int res[n+1] = {0};
+   arr[0] = 0;
+   for( i=1;i<=n;i++)
+      cin >>arr[i];
+   for(i=1;i<=n;i++)
+      res[arr[i]] = i;
+   for(i=1;i<=n;i++)
+      cout << res[i] <<" ";
+}
+int main()
+{
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+  // ll t;
+   //cin >>t;
+   //for(int i=1;i<=t;i++)
+     // {
+   solve();
+
+//}   
+}
