098280e7af0e66b2d38cb1ed2acb8738db5bc2a6
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-07-23 00:31:52 +0530

    Create A. Filling Diamonds.cpp

---
+++ Dynamic programming problems/A. Filling Diamonds.cpp
@@ -0,0 +1,29 @@
+//https://codeforces.com/problemset/problem/1339/A
+//// Created by js0805
+#include <bits/stdc++.h>
+#include<algorithm>
+#include<vector>
+#include<string>
+using namespace std;
+using ll = long long;
+void solve() {
+	int n;
+	cin >> n;
+	cout << n << "\n";
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.in", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
